894dff5b042c5448ef4d995a34791e9b6dce3a6a
Hephzibah <hephziboy007@gmail.com>
2023-07-09 18:49:08 -0700

    first argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This program prints its name
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
